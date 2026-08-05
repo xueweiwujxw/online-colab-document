168365a10bfbd27bb388ddd9cea58e5003c9e471
xueweiwujxw <xueweiwujxw@outlook.com>
2026-08-05 20:41:53 +0800

    fix(frontend): proxy api in dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
   },
 });
